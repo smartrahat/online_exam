bae3fc52039d4a3cecdef9f09da9952fd346628c
Mohammed Rahat Hossain <smartrahat@gmail.com>
2016-11-22 01:47:31 +0600

    again test

--- resources/views/inc/aside.blade.php
+++ resources/views/inc/aside.blade.php
@@ -65,7 +65,6 @@
                document.getElementById('logout-form').submit();">
                 Logout
             </a>
-            <!-- comitted -->
             <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                 {{ csrf_field() }}
             </form>
